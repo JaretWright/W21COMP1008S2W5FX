939329de8a003945c39c74a8c82ca27ba2dc9995
JaretWright <jaret.wright@georgiancollege.ca>
2021-02-18 13:38:07 -0500

    Updated the view and controller to have fx:id's

---
+++ src/controllers/CardViewController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CardViewController {
+    @FXML
+    private Label faceNameLabel;
+
+    @FXML
+    private Label suitLabel;
+
+    @FXML
+    private Label faceValueLabel;
+
+
+}
